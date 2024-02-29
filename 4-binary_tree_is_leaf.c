f59348f8f87d5b195d92cab6ce6a6d251b4203f1
Carol-96 <karolynkalumu@gmail.com>
2024-02-29 10:52:04 -0800

    is root

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,11 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - Deletes an entire binary tree
+ * binary_tree_is_leaf - Check if a node is a leaf.
+ * @node: A pointer to the node to check.
  *
- * @tree: Tree to delete
- * Return: 1 if node is a leaf.
- *         Otherwise - 0
+ * Return: 1 if the node is a leaf, 0 otherwise.
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
